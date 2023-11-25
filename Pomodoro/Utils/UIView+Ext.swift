89de5ce7ea5afc449768b1598991639282d58e36
Bekbol Bolatov <bekbolbolatov@gmail.com>
2023-11-25 15:11:09 +0600

    pin

--- Pomodoro/Utils/UIView+Ext.swift
+++ Pomodoro/Utils/UIView+Ext.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 extension UIView {
+    
+    func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.topAnchor),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor)
+        ])
+    }
+    
     func addSubviews(_ views: UIView...) {
         addSubviews(views)
     }
